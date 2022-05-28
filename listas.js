6b08259c6fd8cdb2d23d10c230fefa85f3383fa1
Mariano Mignelli <marianomignelli999@gmail.com>
2022-05-28 12:51:14 -0300

    Listas Open Bootcamp

---
+++ listas.js
@@ -0,0 +1,18 @@
+const nombre = ["Mariano"]
+const edad = [23]
+const desarrollador = [true]
+const fecha = new Date (2022, 0, 9)
+
+console.log(nombre);
+console.log(edad);
+console.log(desarrollador);
+console.log(fecha);
+
+//Objeto
+const libro = {
+    titulo: "Rayuela",
+    autor: "Mario Benedetti",
+    fecha: (1963, 1, 18)
+}
+
+console.log (libro)
